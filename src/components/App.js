5e65527a3e11e92a987d4725e333723f3c9bb557
Nic Solitom <nicsolitom@gmail.com>
2021-07-22 01:05:33 +0200

    Redesigned navbar and title

--- src/components/App.js
+++ src/components/App.js
@@ -14,33 +14,47 @@ import '../styles/App.scss';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 function App() {
+  const ColoredLine = ({ color }) => (
+    <hr className="WhiteLine"
+        style={{
+            color: color,
+            backgroundColor: color,
+            height: 1
+        }}
+    />
+);
   return (
     <div className="App">
       <header className="App-header">
+        <div className="TitleContainer">
+        <h3 className="MyName">Nic Solitom</h3><h3 className="MyLocation">⋮⋮⋮ Berlin / Earth</h3>
+        <ColoredLine color="white" />
+        <h4 className="MyTitle">design and web development</h4>
+        </div></header>
         <Router>
-          <nav className="navbar navbar-light bg-light">
+          <nav className="navbar">
             <form className="form-inline">
               <NavLink
                 exact
                 to="/"
-                className="NavLink btn btn-outline-light ml-2"
+                className="NavLink btn btn-outline-black ml-2"
                 type="button"
               >
-                Home
-              </NavLink>
-              <NavLink
-                to="/credit"
-                className="NavLink btn btn-outline-light ml-2"
-                type="button"
-              >
-                Credit
+                WORK
               </NavLink>
               <NavLink
                 to="/about"
-                className="NavLink btn btn-outline-light ml-2"
+                className="NavLink btn btn-outline-black ml-2"
                 type="button"
               >
-                About
+                ABOUT ME
+              </NavLink>
+              <NavLink
+                to="/credit"
+                className="NavLink btn btn-outline-black ml-2"
+                type="button"
+              >
+                MY PATH
               </NavLink>
             </form>
           </nav>
@@ -55,7 +69,7 @@ function App() {
             <Route path="*" render={() => <div>404 - Not Found</div>} />
           </Switch>
         </Router>
-      </header>
+      
       <footer className="App-footer">
         <h6> Nic Solitom &copy; 2021</h6>
       </footer>
