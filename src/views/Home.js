0973edd0f5b11f14be2fee66d00510a708b79048
Nic Solitom <nicsolitom@gmail.com>
2021-07-21 22:54:45 +0200

    Edits: Broser title and prep for Home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-import rockStar from '../images/musician-664432_1920.jpg';
 import '../styles/Home.scss'
 
-export default function Home() {
-    return (
-      <div className="HomeContainer">
-        <h5>Welcome to a React-routing page</h5><br />
-        <img src={rockStar} className="RockstarImage" alt="Rockstar"/>
 
+export default function Home() {
+  return (
+    <div className="HomeContainer">
+        <h5>PUT PROJECTS <br></br>IN GRID HERE</h5><br />
+        
 
       </div>
     );
-}
+  }
+  
+
+  // Delete?
+  //import rockStar from '../images/musician-664432_1920.jpg';
+  //<img src={rockStar} className="RockstarImage" alt="Rockstar"/> 
