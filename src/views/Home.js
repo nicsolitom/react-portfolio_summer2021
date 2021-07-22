9e65381f5edd79051f63edd3bd944977b2dd0368
Nic Solitom <nicsolitom@gmail.com>
2021-07-22 03:46:49 +0200

    Unfinished. Added example pics

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,17 +1,63 @@
 import React from 'react';
+// WORK:
+import ToDo from '../images/work/1602179761063.jpg';
+import CA from '../images/work/1626089759147.jpg';
+import Afire from '../images/work/afireafire.png';
+import cleanLast from '../images/work/clean_lastparty_500w.png';
 
-import '../styles/Home.scss'
-
+import '../styles/Home.scss';
 
 export default function Home() {
   return (
-    <div className="HomeContainer">
 
-      </div>
+
+    <div className="ProjectContainer">
+
+        <div className="Projects">
+          <img src={ToDo} />
+        </div>
+
+        <div className="Projects">
+          <img src={CA} />
+        </div>
+
+        <div className="Projects">
+          <img src={Afire} />
+        </div>
+
+        <div className="Projects">
+          <img src={cleanLast} />
+        </div>
+
+    </div>
     );
   }
   
-
+  
   // Delete?
   //import rockStar from '../images/musician-664432_1920.jpg';
   //<img src={rockStar} className="RockstarImage" alt="Rockstar"/> 
+  {/* 
+          <a href="https://www.nicsolitom.com" target="blank">
+              <img src={Clean09}></img>
+            </a> */}
+  
+  
+            {/* <a href="img/oldworks/clean_pride09_800w.png" target="blank">
+              <img src="img/oldworks/clean_pride09_500w.png" alt="Flyer: CLEAN Pride 09">
+            </a>
+  
+            <div class="listenoire"><a href="https://youtu.be/HKwU-eFEN44" target="blank">
+              <img src="img/afireafire.png" alt="Video Editing for Liste Noire"></a></div>
+            
+                      <a href="img/oldworks/mrgaysweden08_630h.png" target="blank">
+                          <img src="img/oldworks/mrgaysweden08_400h.png" alt="Flyer: Mr Gay Sweden 08">
+                      </a>
+            
+                      <a href="img/oldworks/clean_nye_650h.png" target="blank">
+                          <img src="img/oldworks/clean_nye_400h.png" alt="Flyer: CLEAN New Years Eve">
+                      </a>
+            
+                      <a href="img/oldworks/clean_lastparty_800w.png" target="blank">
+                          <img src="img/oldworks/clean_lastparty_500w.png" alt="Flyer: CLEAN - Last Party Of The Year">
+                      </a> */}
